111fd79aca3fb8f23a1bafa2882f1dc20e78c4b9
Gwangyu Lee <fred.lee.9808@icloud.com>
2024-09-12 00:42:46 +0900

    Delete visionOS xcode project

--- KlakNDI-main/visionOS Build/Libraries/com.unity.xr.visionos/Runtime/Plugins/visionos/VisionOSSettings.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-var VisionOSEnableFoveation = true
-var VisionOSUpperLimbVisibility: Visibility = .hidden
-var VisionOSImmersionStyle: ImmersionStyle = .automatic
-var VisionOSSkipPresent = false
-var VisionOSEDRHeadroom = 2
